e67f7dce4ad99ee2b0850e62e9a0921ba61affd6
Maksym STORCHAK <mstorcha@e3r6p10.unit.ua>
2018-07-07 13:04:13 +0300

    initial commit

---
+++ src/main.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstorcha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/23 13:20:54 by mstorcha          #+#    #+#             */
+/*   Updated: 2018/05/23 13:21:42 by mstorcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+
+int		main(int argc, char **argv)
+{
+	t_main	mlx;
+
+	if (argc != 2)
+		return (0);
+	mlx.scene = scene_create(argv);
+	mlx.mlx_ptr = mlx_init();
+	mlx.win_ptr = mlx_new_window(mlx.mlx_ptr, WIDTH, HEIGHT, "RTv1");
+	mlx.image.data = mlx_new_image(mlx.mlx_ptr, HEIGHT, WIDTH);
+	mlx.image.ptr = (int *)mlx_get_data_addr(mlx.image.data,
+			&mlx.image.bits, &mlx.image.sz_l, &mlx.image.endi);
+	rendering(mlx);
+	mlx_hook(mlx.win_ptr, 2, 0, &key, &(mlx));
+	mlx_hook(mlx.win_ptr, 17, 0, (int (*)())exit, NULL);
+	mlx_loop(mlx.mlx_ptr);
+	return (0);
+}
---
+++ src/opencl1.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   opencl.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mstorcha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/19 21:41:38 by mstorcha          #+#    #+#             */
+/*   Updated: 2018/06/19 21:41:40 by mstorcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "rt.h"
+
+void			exit_message(const char *str)
+{
+	ft_putendl(str);
+	exit(1);
+}
+
+static void		free_cl_mem(t_opencl cl)
+{
+	clReleaseMemObject(cl.memobj_data);
+	clReleaseMemObject(cl.memobj_figures);
+	clReleaseMemObject(cl.memobj_light);
+	clReleaseProgram(cl.program);
+	clReleaseKernel(cl.kernel);
+	clReleaseCommandQueue(cl.command_queue);
+	clReleaseContext(cl.context);
+}
+
+void			rendering(t_main mlx)
+{
+	t_opencl	cl;
+	int			memlenth;
+	size_t		global_work_size[3];
+	int			ret;
+
+	memlenth = 1440000;
+	global_work_size[0] = 1200;
+	global_work_size[1] = 1200;
+	global_work_size[2] = 0;
+	mlx_clear_window(mlx.mlx_ptr, mlx.win_ptr);
+	start_cl(&cl);
+	mid_cl(&cl, mlx, memlenth);
+	args_cl(&cl, mlx);
+	if ((ret = clEnqueueNDRangeKernel(cl.command_queue,
+				cl.kernel, 2, NULL, global_work_size, NULL, 0, NULL, NULL)))
+		exit_message("failed to execute kernel");
+	if ((ret = clEnqueueReadBuffer(cl.command_queue,
+				cl.memobj_data, CL_TRUE, 0, memlenth * sizeof(int),
+				mlx.image.ptr, 0, NULL, NULL)))
+		exit_message("failed to get buf data");
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.image.data, 0, 0);
+	free_cl_mem(cl);
+}
